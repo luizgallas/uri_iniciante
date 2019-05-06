a43982dbe98b638fdb885b95d71fea0e105db66b
luizgallas <luizgallas@outlook.com>
2019-05-06 08:39:23 -0300

    remoções

--- IMC.py
+++
@@ -1,4 +0,0 @@
-massa = float(input("Qual a sua massa? "))
-altura = float(input("Qual sua altura? "))
-imc = (massa / (altura * altura))
-print("Seu IMC é: %.2f" % imc)
--- sasa.py
+++
@@ -1,7 +0,0 @@
-a, b, c = map(int, input().split())
-
-ab = (a + b + abs(a - b)) // 2
-
-abc = (ab + c + abs(ab - c)) // 2
-
-print(abc, "é o maior")
